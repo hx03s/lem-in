52e884efbcde4ca5179b7a953c19b43a6385f507
mIna <menaalkhalfan@gmail.com>
2024-05-18 21:22:20 +0300

    ksd

---
+++ func/structs.go
@@ -0,0 +1,37 @@
+package lem
+
+type Room struct {
+	Id      int
+	Name    string
+	CoordX  int
+	CoordY  int
+	IsStart bool
+	IsEnd   bool
+	Visited bool
+	Links   []*Link
+}
+
+type Link struct {
+	Room *Room
+	Next *Link
+}
+
+type AntFarm struct {
+	NumAnts   int
+	StartRoom *Room
+	EndRoom   *Room
+	Rooms     []*Room
+}
+
+type Node struct {
+	Name      string
+	Neighbors []*Node
+}
+
+type Ant struct {
+	Id          int
+	Farm        *AntFarm
+	CurrentNode *Node
+	NextNode    *Node
+	Visited     bool
+}
