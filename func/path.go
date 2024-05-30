1315654465c66625f4f0741f62d3090ab945ce3d
ahmmohammed <Ahmedx39311@gmail.com>
2024-05-30 10:56:20 +0300

    .

--- func/path.go
+++ func/path.go
@@ -6,6 +6,7 @@ func Edmonds(farm *AntFarm) []*Path {
     visited := make(map[*Room]bool)
     queue := []*Path{{Rooms: start}}
     var paths []*Path
+    visited[farm.StartRoom] = true
 
     for len(queue) > 0 {
         path := queue[0]
@@ -16,17 +17,36 @@ func Edmonds(farm *AntFarm) []*Path {
             newPath := &Path{Rooms: make([]*Room, len(path.Rooms))}
             copy(newPath.Rooms, path.Rooms)
             paths = append(paths, newPath)
+
+            // Reset visited status of rooms not part of finalized path
+            for room := range visited {
+                if room == farm.StartRoom {
+                    continue
+                }
+                visited[room] = false
+            }
+            for _, currentpath := range paths {
+                for _, currentroom := range currentpath.Rooms {
+                    if currentroom == farm.EndRoom {
+                        continue
+                    }
+                visited[currentroom] = true
+                } 
+            }
+            queue = queue[:0]
+            queue = []*Path{{Rooms: start}}
             continue
         }
-		if visited[currentRoom] {
-			continue
-		}
+        // if visited[currentRoom] {
+        //     continue
+        // }
 
-        visited[currentRoom] = true
+        // visited[currentRoom] = true
 
         for _, link := range currentRoom.Links {
             nextRoom := link.Room
             if !visited[nextRoom] {
+                visited[nextRoom] = true
                 newPath := &Path{Rooms: make([]*Room, len(path.Rooms), len(path.Rooms)+1)}
                 copy(newPath.Rooms, path.Rooms)
                 newPath.Rooms = append(newPath.Rooms, nextRoom)
